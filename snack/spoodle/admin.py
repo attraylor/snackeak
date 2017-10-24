e4d1afbac15b95889b0f3ee3abe753fe07b29a72
Jay McMahon <Jay McMahon>
2017-10-24 17:15:57 -0400

    models kinda sorta

--- snack/spoodle/admin.py
+++ snack/spoodle/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Class, Student, Todo, Professor, Homework, Note
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Todo)
+admin.site.register(Professor)
+admin.site.register(Homework)
+admin.site.register(Note)
--- snack/spoodle/models.py
+++ snack/spoodle/models.py
@@ -1,3 +1,87 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Class(models.Model):
+    """
+    Model representing a Class.
+    """
+    name = models.CharField(max_length=200)
+    
+    
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.name
+		
+class Student(models.Model):
+    """
+    Model representing a student.
+    """
+    name = models.CharField(max_length=200)
+    classes = models.ManyToManyField(Class)
+    id = models.CharField('ID',max_length=8, help_text='8 digit Student ID number',primary_key=True)
+    
+    
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.name
+		
+class Todo(models.Model):
+    """
+    Model representing a todo item (e.g. mow the lawn).
+    """
+    student = models.ForeignKey('Student')
+    activity = models.CharField(max_length=200, help_text="Enter a task you have to do.")
+    
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.activity
+		
+class Professor(models.Model):
+    """
+    Model representing a professor.
+    """
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10, help_text='Ten digit phone number, no dashes')
+    office = models.CharField(max_length=100)
+    hours = models.CharField(max_length=100)
+    classes= models.ForeignKey(Class)
+    
+    
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.name
+		
+class Homework(models.Model):
+    """
+    Model representing a homework assignment.
+    """
+    title = models.CharField(max_length=200)
+    duedate = models.DateField()
+    classes = models.ForeignKey(Class)
+    
+    
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+		
+class Note(models.Model):
+    """"
+	Model representing a class note
+	"""
+    title = models.CharField(max_length=100)
+    note = models.TextField()
+    classes = models.ForeignKey(Class)
+
