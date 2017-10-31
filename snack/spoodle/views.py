2aad82c0f614092ce87c49dfb7e6e95646f79e7b
Fred <aaron@1x-nat-vl931-172-30-172-150.wireless.umass.edu>
2017-10-31 13:33:31 -0400

    todo page

--- snack/spoodle/views.py
+++ snack/spoodle/views.py
@@ -10,10 +10,26 @@ def index(request):
     classes = Class.objects.all()
 
 
-    
+
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
         context={'classes':classes},
+    )
+
+
+def todo(request):
+    """
+    View function for home page of site.
+    """
+    todos = Todo.objects.all()
+
+
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'todo.html',
+        context={'todos':todos},
     )
